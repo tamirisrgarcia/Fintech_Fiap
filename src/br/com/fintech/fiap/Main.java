c1eeb3176aa2f55ee29900acdf1101e75be28943
TAMIRIS REZENDE GARCIA COSTA <tamirisrgarcia@gmail.com>
2024-03-07 20:16:18 -0300

    ajuste do nome da classe Main

--- src/br/com/fintech/fiap/Main.java
+++ src/br/com/fintech/fiap/Main.java
@@ -2,7 +2,7 @@ package br.com.fintech.fiap;
 
 import java.util.Date;
 
-public class Fintech {
+public class Main {
 	public static void main(String[] args) {
 		Usuario user = new Usuario(123, "Teste", "email@email.com", new Date(1995-12-24), "foto.png");
 		
