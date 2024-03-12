33898d94f995bf56ae0397cb2e621fdc720166d7
João Vitor S <jvsg27@hotmail.com>
2024-03-12 10:40:04 -0300

    Metodos cartao + endereco
    Implementação dos metodos nas classes cartao e endereco.

--- src/br/com/fintech/fiap/Cartao.java
+++ src/br/com/fintech/fiap/Cartao.java
@@ -1,20 +1,21 @@
 package br.com.fintech.fiap;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class Cartao {
 
 	public int id_cartao;
 	private int nr_cartao;
-	private Date validade;
+	private String validade;
 	private double limite;
 	private String titular;
 	private String bandeira;
 	private int nr_seguranca;
 	private String tipo;
+	static Cartao cartao;
 	
-	public Cartao(int id_cartao, int nr_cartao, Date validade, double limite, String titular, String bandeira,
-			int nr_seguranca, String tipo) {
+	public Cartao(int id_cartao, int nr_cartao, String validade, double limite, String titular, String bandeira, int nr_seguranca, String tipo) {
 		super();
 		this.id_cartao = id_cartao;
 		this.nr_cartao = nr_cartao;
@@ -26,11 +27,11 @@ public class Cartao {
 		this.tipo = tipo;
 	}
 
-	public Date getValidade() {
+	public String getValidade() {
 		return validade;
 	}
 
-	public void setValidade(Date validade) {
+	public void setValidade(String validade) {
 		this.validade = validade;
 	}
 
@@ -87,15 +88,66 @@ public class Cartao {
 		this.nr_seguranca = nr_seguranca;
 	}
 	
-	private void incluirCartao() {
-		
+
+	public static void incluirCartao(int id_cartao, int nr_cartao, String validade, double limite, String titular, String bandeira, int nr_seguranca, String tipo) {
+		cartao = new Cartao(id_cartao, nr_cartao, validade, limite, titular, bandeira, nr_seguranca, tipo);
+		System.out.println("Cartão adicionado com sucesso !");
 	}
 	
-	public void consultarCartao(int id_cartao) {
-		
+	public void consultarCartao() {
+		System.out.println("Numero do cartão: " + nr_cartao + " \nValidade: " + validade + "\nLimite: " + limite + "\nTitular: " + titular + "\nBandeira: " + bandeira + "\nNumero de segurança: " + nr_seguranca + "\nTipo: " + tipo);
 	}
 	
-	private void editarCartao(int id_cartao) {
+	public void editarCartao() {
+		System.out.print("1 - Editar número do cartão:\n2 - Editar validade:\n3 - Editar titular:\n4 - Editar Limite:\n5 - Editar Bandeira;\n6 - Editar número de segurança:\n7 - Editar tipo:");
+		Scanner sc = new Scanner(System.in);
+		int resposta = sc.nextInt();
+		
+		if(resposta == 1) {
+			System.out.println("Digite o novo número do cartão: ");
+			int novoNumero = sc.nextInt();
+			nr_cartao = novoNumero;
+			System.out.println("Seu novo número do cartão é: " + nr_cartao);
+		} 
+		else if(resposta == 2 ) {
+			System.out.println("Digite a nova data de validade: ");
+			String novaValidade = sc.next();
+			validade = novaValidade;
+			System.out.println("Sua nova validade é: " + validade);
+		} 
+		else if(resposta == 3) {
+			System.out.println("Digite o(a) novo(a) titular: ");
+			String novoTitular = sc.next();
+			titular = novoTitular;
+			System.out.println("Seu novo tipo de conta é: " + titular);
+		} 
+		else if(resposta == 4) {
+			System.out.println("Digite o novo limite: ");
+			double novoLimite = sc.nextDouble();
+			limite = novoLimite;
+			System.out.println("Seu novo limite é: " + limite);
+		}
+		else if(resposta == 5) {
+			System.out.println("Digite a nova bandeira: ");
+			String novaBandeira = sc.next();
+			bandeira = novaBandeira;
+			System.out.println("Sua nova bandeira é: " + novaBandeira);
+		}
+		else if(resposta == 6) {
+			System.out.println("Digite o novo número de segurança: ");
+			int novoNrSeguranca = sc.nextInt();
+			nr_seguranca = novoNrSeguranca;
+			System.out.println("Seu novo tipo de conta é: " + novoNrSeguranca);
+		}
+		else if(resposta == 7) {
+			System.out.println("Digite o novo tipo do cartão: ");
+			String novoTipo = sc.next();
+			tipo = novoTipo;
+			System.out.println("Seu novo tipo de cartão é: " + novoTipo);
+		}
+		else {
+			System.out.println("Opção inválida");
+		}
 		
 	}
 	
@@ -107,8 +159,9 @@ public class Cartao {
 		
 	}
 	
-	public void excluirCartao(int id_cartao) {
-		
+	public static void excluirCartao(int id_cartao) {
+		cartao = null;
+		System.out.println("Cartão excluido com sucesso!");
 	}
 	
 }
--- src/br/com/fintech/fiap/Main.java
+++ src/br/com/fintech/fiap/Main.java
@@ -5,6 +5,12 @@ import java.util.Date;
 public class Main {
 	public static void main(String[] args) {
 		
+		Endereco.incluirEndereco(1, "Brasil", "São Paulo", "São Paulo", "Parque São Lucas", "Rosario do Catete", 477);
+
+		Endereco.endereco.consultarEndereco();
 		
+		Endereco.endereco.editarEndereco();
+		Endereco.endereco.consultarEndereco();
+
 	}
 }
