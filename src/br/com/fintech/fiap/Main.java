5f3de799af6dda02749d580ce66ff45864f341d3
João Vitor S <jvsg27@hotmail.com>
2024-03-14 11:57:43 -0300

    Suporte

--- src/br/com/fintech/fiap/Main.java
+++ src/br/com/fintech/fiap/Main.java
@@ -2,8 +2,11 @@ package br.com.fintech.fiap;
 
 public class Main {
 	public static void main(String[] args) {
+		Funcionario.incluirFuncionario(123, "testen", "testem", 1234, "testec", 456, 23, "2786", "testef");
+
+		Suporte.solicitarAtendimento(123, 0, "testedt", 2, "x dias uteis", "testect", false);
 		
-		
+		Suporte.suporte.consultarAtendimento();
 		
 	}
 }
